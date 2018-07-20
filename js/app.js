9c180f1337047509bb5fc0c10f138f0d28bc3957
allenfortner <allenfortner99@gmail.com>
2018-07-20 12:32:08 -0400

    Add player class and initialize player object

--- js/app.js
+++ js/app.js
@@ -24,7 +24,18 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+	constructor() {
+		this.sprite = 'images/char-boy.png';
+		this.x = 0;
+		this.y = 0;
+	}
+	render() {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+}
 
+const player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
